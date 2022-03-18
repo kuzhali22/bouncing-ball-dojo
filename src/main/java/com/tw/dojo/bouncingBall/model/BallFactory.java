8bcb781ef3514c0ea05c8d3aaafc948cd353c88d
Prapulla Bommareddy <prapulla.bommareddy@gmail.com>
2022-03-18 14:27:41 +0530

    [Sheetal | Prapulla] Add. New functinality bouncing elastic ball added.

--- src/main/java/com/tw/dojo/bouncingBall/model/BallFactory.java
+++ src/main/java/com/tw/dojo/bouncingBall/model/BallFactory.java
@@ -6,8 +6,12 @@ public class BallFactory {
         return new Ball[]{
                 bouncingBall(75, 50, BouncingBall.DOWN),
                 elasticBall(250, 100, Ball.DEFAULT_RADIUS, ElasticBall.SHRINK),
-                // bouncingElasticBall() --> Let's make a new ball!
+                bouncingElasticBall(500, 100, BouncingBall.DOWN, Ball.DEFAULT_RADIUS, ElasticBall.SHRINK) //--> Let's make a new ball!
         };
+    }
+
+    private static Ball bouncingElasticBall(int centerX, int centerY, int direction, int radius, int resize) {
+        return new BouncingElasticBall(centerX, centerY, direction, radius, resize);
     }
 
     public static Ball bouncingBall(int centerX, int centerY, int direction) {
---
+++ src/main/java/com/tw/dojo/bouncingBall/model/BouncingElasticBall.java
@@ -0,0 +1,107 @@
+package com.tw.dojo.bouncingBall.model;
+
+import com.tw.dojo.bouncingBall.ui.BallWorld;
+
+public class BouncingElasticBall extends Ball {
+    static final int DOWN = 1;
+    static final int UP = -1;
+
+    private int direction;
+    public static final int GROWTH_RATE = 2;
+    public static final int MOVEMENT_SPEED = 12;
+    static final int GROW = 1;
+    static final int SHRINK = -1;
+
+    private int growthDirection;
+
+    public BouncingElasticBall(int centerX, int centerY, int direction, int radius, int resize) {
+        super(centerX, centerY);
+        this.direction = direction;
+        this.growthDirection = resize;
+    }
+
+    @Override
+    public void update() {
+        direction = reverseDirectionIfNecessary();
+        y = move();
+        growthDirection = reverseGrowthDirectionIfNecessary();
+        radius = next();
+
+    }
+
+    /***********************************************************************************
+     *
+     * Do not change Bouncing ALGORITHM below.
+     *
+     ***********************************************************************************/
+
+    private int reverseDirectionIfNecessary() {
+        if (movingTooHigh() || movingTooLow()) {
+            return switchDirectionOfMovement();
+        }
+
+        return this.direction;
+    }
+
+    private boolean movingTooLow() {
+        return y + radius >= BallWorld.BOX_HEIGHT && movingDown();
+    }
+
+    private boolean movingTooHigh() {
+        return y - radius <= 0 && movingUp();
+    }
+
+    private int switchDirectionOfMovement() {
+        return movingDown() ? UP : DOWN;
+    }
+
+    private int move() {
+        return y + (MOVEMENT_SPEED * direction);
+    }
+
+    private boolean movingDown() {
+        return direction == DOWN;
+    }
+
+    private boolean movingUp() {
+        return direction == UP;
+    }
+
+    /***********************************************************************************
+     *
+     * Do not change Elastic ALGORITHM below.
+     *
+     ***********************************************************************************/
+
+    private int reverseGrowthDirectionIfNecessary() {
+        if (growingTooBig() || shrinkingTooSmall()) {
+            return switchDirectionOfGrowth();
+        }
+
+        return this.growthDirection;
+    }
+
+    private boolean shrinkingTooSmall() {
+        return radius <= 0 && shrinking();
+    }
+
+    private boolean growingTooBig() {
+        return radius >= DEFAULT_RADIUS && growing();
+    }
+
+    private int switchDirectionOfGrowth() {
+        return growing() ? SHRINK : GROW;
+    }
+
+    private int next() {
+        return radius + (GROWTH_RATE * growthDirection);
+    }
+
+    private boolean shrinking() {
+        return growthDirection == SHRINK;
+    }
+
+    private boolean growing() {
+        return growthDirection == GROW;
+    }
+}
